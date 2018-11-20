bc736e418f492e434274f6f4244dd9c2bfb04164
Acayaba <bianca.acayaba@gmail.com>
2018-11-20 16:40:04 +0000

    finaL

--- index.php
+++ index.php
@@ -1,46 +1,42 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <meta http-equiv="Content-Security-Policy" content="default-src * data: gap: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
-    <link rel="stylesheet" href="style.css">
-    <script src="jquery-3.3.1.min.js"></script>
-    <script src="calcula.js"></script> 
-    <link rel="stylesheet" href="css/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta http-equiv="Content-Security-Policy" content="default-src * data: gap: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+<link rel="stylesheet" href="style.css">
+<script src="jquery-3.3.1.min.js"></script>
+<script src="calcula.js"></script> 
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
 <div class="container">
-    <header class="telaresult">
-        <form action="" method="post">
-            <input type="text" id="resultado" class="corpo" readonly>
-        </form>
-    </header>
-    
-    <section class="digita">
-        <input type="button" value="1" name="btn" class="btn">
-        <input type="button" value="2" name="btn" class="btn">
-        <input type="button" value="3" name="btn" class="btn">
-        <input type="button" value="+" name="soma" class="btn" style="background-color: gray; color:white;">
-        <input type="button" value="C" name="c" class="btn">
-        <input type="button" value="4" name="btn" class="btn">
-        <input type="button" value="5" name="btn" class="btn">
-        <input type="button" value="6" name="btn" class="btn">
-        <input type="button" value="-" name="subtracao" class="btn" style="background-color: gray;color:white;">
-        <input type="button" value="CE" name="ce" class="btn">
-        <input type="button" value="7" name="btn" class="btn">
-        <input type="button" value="8" name="btn" class="btn">
-        <input type="button" value="9" name="btn" class="btn">
-        <input type="button" value="*" name="multiplicacao" class="btn" style="background-color: gray;color:white;">
-        <input type="button" value="=" name="igual" class="btn igual">
-        <input type="button" value="0" name="btn" class="btn zero">
-        <input type="button" value="." name="btn" class="btn">
-        <input type="button" value="÷" name="divisao" class="btn" style="background-color: gray;color:white;">
-        
-    </section>
+<header class="telaresult">
+ <form action="" method="post">
+<input type="text" id="resultado" class="corpo" readonly>
+ </form>
+ </header>
+<section class="digita">
+<input type="button" value="1" name="btn" class="btn">
+<input type="button" value="2" name="btn" class="btn">
+<input type="button" value="3" name="btn" class="btn">
+<input type="button" value="+" name="soma" class="btn" style="background-color: gray; color:white;">
+<input type="button" value="C" name="c" class="btn">
+<input type="button" value="4" name="btn" class="btn">
+<input type="button" value="5" name="btn" class="btn">
+<input type="button" value="6" name="btn" class="btn">
+<input type="button" value="-" name="subtracao" class="btn" style="background-color: gray;color:white;">
+<input type="button" value="CE" name="ce" class="btn">
+<input type="button" value="7" name="btn" class="btn">
+<input type="button" value="8" name="btn" class="btn">
+<input type="button" value="9" name="btn" class="btn">
+<input type="button" value="*" name="multiplicacao" class="btn" style="background-color: gray;color:white;">
+<input type="button" value="=" name="igual" class="btn igual">
+<input type="button" value="0" name="btn" class="btn zero">
+<input type="button" value="." name="btn" class="btn">
+<input type="button" value="÷" name="divisao" class="btn" style="background-color: gray;color:white;">
+</section>
 </div>
-    
 </body>
 </html>
